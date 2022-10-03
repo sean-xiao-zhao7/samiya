f9314e7426bc62be19d4b352ec3f4b9a343c3ebe
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-10-03 16:16:09 -0400

    Add home image

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,24 @@
 import Header from "../layout/Header";
 import Footer from "../layout/Footer";
 
+// images
+import homeWide from "../../assets/photos/home_wide.png";
+
 const Home = () => {
     return (
         <div id="home">
             <Header />
-            <div id="home_body">Home page</div>
+            <div id="home_body">
+                <section id="hero">
+                    <img src={homeWide} alt="samiya people" />
+                    <section>
+                        Samiya exists to help underprivileged children, youth,
+                        and women to rise out of abject poverty and to become
+                        kingdom minded Christians who successfully contribute to
+                        God's global economy.
+                    </section>
+                </section>
+            </div>
             <Footer />
         </div>
     );
