2db5ed7dbf12f96926fde4444c166f932ffbf66b
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-10-03 13:59:19 -0400

    Add header and sass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 
 // pages
 import Home from "./components/pages/Home";
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,18 @@
+import samiyaLogo from "../../assets/logos/samiya_logo.png";
+
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header>
+            <img src={samiyaLogo} alt="Samiya logo" />
+            <ul>
+                <li>About Us</li>
+                <li>Pray</li>
+                <li>Projects</li>
+                <li>Donate</li>
+                <li>Contact</li>
+            </ul>
+        </header>
+    );
 };
 
 export default Header;
