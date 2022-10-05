d06bd5af95bc0876b33914e7399c207760d958d4
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-10-05 15:38:09 -0400

    Complete pray page

--- src/components/pages/Pray.jsx
+++ src/components/pages/Pray.jsx
@@ -3,7 +3,7 @@ import Header from "../layout/Header";
 import Footer from "../layout/Footer";
 
 // images
-import homeWide from "../../assets/photos/home_wide.png";
+import homeWide from "../../assets/photos/horizontal_praying.png";
 
 const Pray = () => {
     return (
@@ -13,21 +13,71 @@ const Pray = () => {
                 <section id="hero">
                     <img src={homeWide} alt="samiya people" />
                     <section>
-                        Samiya International is a global mission agency
-                        committed to church planting among the poor and the
-                        marginalized in unreached nations around the world.
-                        <button type="button">Learn more</button>
-                        <button type="button">Support us</button>
+                        <h1>Prayer is Vital to Global Missions</h1>
+                        Prayer for global missions changes our hearts as we
+                        learn more about the world and rejoice in lives changed
+                        by the gospel.
+                        <button type="button">
+                            Current prayers <span>⇩</span>
+                        </button>
+                        <button type="button">Donate</button>
                     </section>
                 </section>
             </div>
-            <section id="this-month">
-                <h1>Praying for Builders</h1>
-                Pray for builders to come and build new buildings for our
-                children in the slums. Pray that God would burden short term
-                teams from North American and Europe to come to Africa and build
-                schools and homes where the kids we support can live and attend
-                school.
+            <section id="current-prayers">
+                <section>
+                    <h1>Praying for Builders</h1>
+                    Pray for builders to come and build new buildings for our
+                    children in the slums. Pray that God would burden short term
+                    teams from North American and Europe to come to Africa and
+                    build schools and homes where the kids we support can live
+                    and attend school.
+                </section>
+                <section>
+                    <h1>Lift up the financial needs of Samiya</h1>
+                    Lift up the financial needs of Samiya International. We are
+                    praying and trusting God for more financial support so that
+                    we can continue our ministries. We are specifically trusting
+                    God for the finances to pay for 1 year of rent on our office
+                    and for 1 year of rent on our directors home in order to
+                    allow our project to be sustainable.
+                </section>
+            </section>
+            <section id="specific-prayers">
+                <section>
+                    <h1>More to pray for </h1>
+                    <ul>
+                        <li>
+                            Join us in praying for our missionary families, new
+                            believers, and the work of planting new churches
+                            among unreached people groups.
+                        </li>
+                        <li>
+                            Form a prayer group that meets regularly. Use the
+                            monthly prayer calendar as well as updates you
+                            receive from Samiya International to form a prayer
+                            team that consistently and routinely blesses our
+                            ministry.
+                        </li>
+                        <li>
+                            Take a different kind of mission trip and pray
+                            on-site at one of our Samiya International field
+                            locations. Locations vary each year – contact us for
+                            details!
+                        </li>
+                        <li>
+                            Pray for missionaries, specifically that God will
+                            continue to make us aware of the needs of the people
+                            we serve and how to most effectively reach them with
+                            the Good News.
+                        </li>
+                        <li>
+                            Pray for the world—that God’s glory will be known
+                            among the unreached, the suffering, the poor, and
+                            the persecuted.
+                        </li>
+                    </ul>
+                </section>
             </section>
             <Footer />
         </div>
